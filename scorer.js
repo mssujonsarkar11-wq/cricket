50a5baf777209b6380264ad6feea115f87b8dec5
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 12:31:50 +0600

    scorer.js

--- scorer.js
+++ scorer.js
@@ -4,27 +4,40 @@ let runs=0
 let wickets=0
 let ball=0
 let over=0
-let totalOvers=0
-let innings=1
+
+let batsman1=""
+let batsman2=""
+let bowler=""
 
 window.startMatch=function(){
 
-totalOvers=parseInt(document.getElementById("overs").value)
+const team=document.getElementById("team").value
+
+batsman1=document.getElementById("batsman1").value
+batsman2=document.getElementById("batsman2").value
+bowler=document.getElementById("bowler").value
 
 set(ref(db,"match"),{
-team:document.getElementById("team").value,
+team:team,
 runs:0,
 wickets:0,
 over:0,
-ball:0
+ball:0,
+batsman1:batsman1,
+batsman2:batsman2,
+bowler:bowler
 })
+
+runs=0
+wickets=0
+ball=0
+over=0
 
 }
 
 window.run=function(r){
 
 runs+=r
-
 ball++
 
 if(ball==6){
@@ -32,7 +45,9 @@ ball=0
 over++
 }
 
-updateScore(r==4?"FOUR":r==6?"SIX":r)
+let event=r==4?"FOUR":r==6?"SIX":r
+
+updateScore(event)
 
 }
 
@@ -53,7 +68,38 @@ updateScore("WICKET")
 window.noball=function(){
 
 runs+=1
+
 updateScore("NO BALL")
+
+}
+
+window.changeBowler=function(){
+
+bowler=prompt("New Bowler Name")
+
+update(ref(db,"match"),{
+bowler:bowler
+})
+
+}
+
+window.newBatsman=function(){
+
+batsman1=prompt("New Batsman Name")
+
+update(ref(db,"match"),{
+batsman1:batsman1
+})
+
+}
+
+window.endInnings=function(){
+
+let target=runs+1
+
+update(ref(db,"match"),{
+target:target
+})
 
 }
 
