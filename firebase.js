d5cfe4c83fb035ca4b6ea3896f0e1cd9783759ba
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 12:06:41 +0600

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+import { getDatabase, ref, set, update, onValue } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
-// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyD1UHEQxowIlbs8CykLfzyEDVg8Ygj0ZAU",
-  authDomain: "cricket-score-c5c9f.firebaseapp.com",
-  projectId: "cricket-score-c5c9f",
-  storageBucket: "cricket-score-c5c9f.firebasestorage.app",
-  messagingSenderId: "5554849346",
-  appId: "1:5554849346:web:26d98739b85220fcbfcfdb"
+ apiKey: "AIzaSyD1UHEQxowIlbs8CykLfzyEDVg8Ygj0ZAU",
+ authDomain: "cricket-score-c5c9f.firebaseapp.com",
+ databaseURL: "https://cricket-score-c5c9f-default-rtdb.firebaseio.com/",
+ projectId: "cricket-score-c5c9f",
+ storageBucket: "cricket-score-c5c9f.appspot.com",
+ messagingSenderId: "5554849346",
+ appId: "1:5554849346:web:26d98739b85220fcbfcfdb"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Export for other modules
-export { db, ref, set, onValue };
+export {db,ref,set,update,onValue};
